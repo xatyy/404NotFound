42f40044b5ac7bf40253f2f9d71f260794bdc7ca
Your Name <you@example.com>
2023-04-24 15:31:00 +0300

    Navigation Fixed

--- CO_GUI/src/main/java/ro/notfound/co_gui/HelloController.java
+++ CO_GUI/src/main/java/ro/notfound/co_gui/HelloController.java
@@ -1,7 +1,9 @@
 package ro.notfound.co_gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -13,15 +15,25 @@ public class HelloController {
     @FXML
     private Label welcomeText;
     private Stage stage;
+    private Scene scene;
 
     @FXML
-    protected void onHelloButtonClick() throws IOException {
-        System.out.println("TEST");
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("test-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = new Stage();
-        stage.setTitle("Salut!");
+    protected void switchtoCPU(ActionEvent TO_CPU) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CPU_Scene.fxml"));
+        stage = (Stage) ((Node)TO_CPU.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
+
+
+    }
+    @FXML
+    protected void switchtoRAM(ActionEvent TO_RAM) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("RAM_Scene.fxml"));
+        stage = (Stage) ((Node)TO_RAM.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
