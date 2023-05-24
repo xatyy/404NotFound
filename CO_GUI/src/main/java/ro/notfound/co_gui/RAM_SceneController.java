5a07864e25814757fa7315ba9303c86f8690b0b1
jgeoo <jacgeorgeeugen@gmail.com>
2023-05-24 20:41:40 +0300

    RAM UI done/Needs algorithm implementation

--- CO_GUI/src/main/java/ro/notfound/co_gui/RAM_SceneController.java
+++ CO_GUI/src/main/java/ro/notfound/co_gui/RAM_SceneController.java
@@ -1,5 +1,6 @@
 package ro.notfound.co_gui;
 
+import com.sun.management.OperatingSystemMXBean;
 import javafx.event.ActionEvent;
 
 
@@ -8,13 +9,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.Scanner;
 
 public class RAM_SceneController {
     private Stage stage;
     private Scene scene;
+    @FXML
+    private Label myLabelProcessor ;
+    @FXML
+    private Label myLabelOS ;
+    @FXML
+    private Label myLabelMemory ;
+    @FXML
+    private Label myLabelUsername;
 
     @FXML
     protected void History(ActionEvent history) throws IOException {
@@ -43,5 +56,15 @@ public class RAM_SceneController {
         stage.show();
 
     }
+    @FXML
+    protected void PC_Specs(ActionEvent TO_Specs) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Specs_RAM.fxml"));
+        stage = (Stage) ((Node) TO_Specs.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
 
 }
